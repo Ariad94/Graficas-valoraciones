396d6f0deb071d1cdbe978a6573e1757661c3fcc
Ariad94 <ariad.94@hotmail.com>
2018-05-23 08:46:15 +0200

    Treemap de medias y de ponderaciones

--- treemap_correlaciones.r
+++ treemap_correlaciones.r
@@ -26,4 +26,21 @@ datos %>% ggplot(aes(area= Medias, fill = Medias, label = Nombres, subgroup = Gr
  strip.text.y = element_text(color = "dodgerblue4", size=15))
 
 
+### ### Diagrama de arbol de competencias, conocimiento y funciones según ponderaciones teoricas
+
+Nombres <- c("Calidad y mejora continua","Pensamiento analítico","Capacidad de aprendizaje",
+ "Solución de problemas","Competencia digital","Lenguajes de programación","Herramientas de desarrollo (id)",
+ "Programar aplicaciones","Manejar bases de datos","Asesorar a los usuarios",
+ "Documentar los módulos","Aplicar las normas")
+Ponderaciones <- c(0.15, 0.2, 0.15, 0.25, 0.25, 0.6, 0.4, 0.3, 0.25, 0.15, 0.2, 0.1)
+Grupos <- c(rep("COMPETENCIAS", 5), rep("CONOCIMIENTOS",2), rep("DESEMPEÑO",5))
+
+datos <- data.frame(Grupos, Nombres, Ponderaciones)
+
+datos %>% ggplot(aes(area= Ponderaciones, fill = Ponderaciones, label = Nombres, subgroup = Grupos)) + geom_treemap() +
+ geom_treemap_text(colour = "white", place = "centre", grow = FALSE) +
+ facet_grid(Grupos~.) + labs(title="Importancia de las competencias, conocimientos y funciones en el perfil") +
+ theme_minimal() + theme(plot.title = element_text( color = "dodgerblue4", size=15, hjust=0, face="bold"),
+ strip.text.y = element_text(color = "dodgerblue4", size=15))
+
 
