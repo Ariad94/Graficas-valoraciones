a79f7e2b485797e1d17df3f189eed725861401e6
Ariad94 <ariad.94@hotmail.com>
2018-06-01 11:16:30 +0200

    Grafico de correlaciones
    Correlaciones por tamaño, etiqueta y degradación de color

--- treemap_correlaciones.r
+++ treemap_correlaciones.r
@@ -4,6 +4,7 @@ library(ggplot2)
 library(dplyr)
 library(ggthemes)
 library(treemapify)
+library(ggcorrplot)
 
 ### ### Diagrama de arbol de competencias, conocimiento y funciones
 
@@ -22,7 +23,7 @@ datos <- data.frame(Grupos, Nombres, Medias)
 datos %>% ggplot(aes(area= Medias, fill = Medias, label = Nombres, subgroup = Grupos)) + geom_treemap() +
  geom_treemap_text(colour = "white", place = "centre", grow = FALSE) +
  facet_grid(Grupos~.) + labs(title="Puntuaciones medias de las competencias, conocimientos y funciones") +
- theme_minimal() + theme(plot.title = element_text( color = "dodgerblue4", size=15, hjust=0, face="bold"),
+ theme_minimal() + theme(plot.title = element_text( color = "dodgerblue4", size=25, hjust=0, face="bold"),
  strip.text.y = element_text(color = "dodgerblue4", size=15))
 
 
@@ -42,5 +43,28 @@ datos %>% ggplot(aes(area= Ponderaciones, fill = Ponderaciones, label = Nombres,
  facet_grid(Grupos~.) + labs(title="Importancia de las competencias, conocimientos y funciones en el perfil") +
  theme_minimal() + theme(plot.title = element_text( color = "dodgerblue4", size=15, hjust=0, face="bold"),
  strip.text.y = element_text(color = "dodgerblue4", size=15))
+
+
+### ### Gráfica de correlaciones 
+# Competencias, conocimientos, funciones
+
+datos  <- data.frame(programadores$C1, programadores$C2, programadores$C3, programadores$C4, programadores$C5,
+ programadores$CN1, programadores$CN2, programadores$F1, programadores$F2, programadores$F3, programadores$F4, programadores$F5)
+
+colnames(datos) <- c("COMPETENCIA \nCalidad y mejora continua","COMPETENCIA \nPensamiento analítico","COMPETENCIA \nCapacidad de aprendizaje",
+ "COMPETENCIA \nSolución de problemas","COMPETENCIA \nCompetencia digital","CONOCIMIENTO \nLenguajes de programación","CONOCIMIENTO \nHerramientas de desarrollo (id)",
+ "FUNCIÓN \nProgramar aplicaciones","FUNCIÓN \nManejar bases de datos","FUNCIÓN \nAsesorar a los usuarios",
+ "FUNCIÓN \nDocumentar los módulos","FUNCIÓN \nAplicar las normas")
+
+coeficientes <- round(cor(datos, method = "spearman"), 1)
+
+ggcorrplot(coeficientes, hc.order = TRUE, type = "lower", lab = TRUE, lab_size = 3, method="circle", 
+ colors = c("palegreen3", "white", "dodgerblue4")) +
+ labs(title="Correlaciones", subtitle = "Coeficientes de correlación de Spearman", x = "", y = "")+ 
+ theme_minimal() +
+ theme(plot.title = element_text( color = "dodgerblue4", size=25, hjust=0),
+ plot.subtitle= element_text(color = "dodgerblue4", size=20), legend.position="none", axis.text.x = element_text(angle = 90, hjust = 1))
+
+
 
 
