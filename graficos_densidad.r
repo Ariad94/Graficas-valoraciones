1911f03a18c3efe260f8fdc2c7edd832dbb3f90d
Ariad94 <ariad.94@hotmail.com>
2018-06-01 10:27:21 +0200

    Graficas de densidad
    Reordenación de los módulos + gráfica de densidades apiladas

--- graficos_densidad.r
+++ graficos_densidad.r
@@ -4,7 +4,62 @@ library(ggplot2)
 library(dplyr)
 library(ggthemes)
 
-### ### Densidad por competencias, conocimientos o funciones 
+### ### Densidades con facet_grid 
+
+#Competencias
+
+Nombre.Competencias <- c(rep("Calidad y mejora continua", 55), rep("Pensamiento analítico", 55), rep("Capacidad de aprendizaje", 55),
+ rep("Solución de problemas", 55), rep("Competencia digital", 55))
+Puntuaciones.Competencias <- c(programadores$C1II,programadores$C2II,programadores$C3II,programadores$C4II,programadores$C5II)
+
+datos <- data.frame(Nombre.Competencias, Puntuaciones.Competencias)
+
+datos %>% ggplot(aes(Puntuaciones.Competencias, ..count.., fill = Nombre.Competencias)) + 
+ geom_density( alpha = 0.25, bw = 0.5, color = "white") + 
+ geom_histogram(binwidth = 0.005) + stat_bin(binwidth=1, geom="text", aes(label=..count..), hjust = -0.5) +  
+ facet_grid(Nombre.Competencias~.) +
+ labs(title="Competencias", subtitle="Número de empleados por puntuación", x = "Puntuaciones", y = "Número de empleados") +
+ theme_pander() + theme(plot.title = element_text( color = "dodgerblue4", size=25, hjust=0),
+ plot.subtitle = element_text( color = "dodgerblue4", size=20, hjust=0),
+ strip.text.y = element_text(color = "dodgerblue4", size=10), legend.position="none")
+
+#Conocimientos
+
+Nombre.Conocimientos <- c(rep("Lenguajes de programación", 55), rep("Herramientas de desarrollo (id)", 55))
+Puntuaciones.Conocimientos <- c(programadores$CN1,programadores$CN2)
+
+datos <- data.frame(Nombre.Conocimientos, Puntuaciones.Conocimientos)
+
+datos %>% ggplot(aes(Puntuaciones.Conocimientos, ..count.., fill = Nombre.Conocimientos)) + 
+ geom_density( alpha = 0.25, bw = 0.5, color = "white") + 
+ geom_histogram(binwidth = 0.005) + stat_bin(binwidth=1, geom="text", aes(label=..count..), hjust = -0.5) +  
+ facet_grid(Nombre.Conocimientos~.) +
+ labs(title="Conocimientos", subtitle="Número de empleados por puntuación", x = "Puntuaciones", y = "Número de empleados") +
+ theme_pander() + theme(plot.title = element_text( color = "dodgerblue4", size=25, hjust=0),
+ plot.subtitle = element_text( color = "dodgerblue4", size=20, hjust=0),
+ strip.text.y = element_text(color = "dodgerblue4", size=15), legend.position="none")
+
+#Competencias
+
+Nombre.Funciones <- c(rep("Programar aplicaciones", 55), rep("Manejar bases de datos", 55), rep("Asesorar a los usuarios", 55),
+ rep("Documentar los módulos", 55), rep("Aplicar las normas", 55))
+Puntuaciones.Funciones <- c(programadores$F1,programadores$F2,programadores$F3,programadores$F4,programadores$F5)
+
+datos <- data.frame(Nombre.Funciones, Puntuaciones.Funciones)
+
+datos %>% ggplot(aes(Puntuaciones.Funciones, ..count.., fill = Nombre.Funciones)) + 
+ geom_density( alpha = 0.25, bw = 0.5, color = "white") + 
+ geom_histogram(binwidth = 0.005) + stat_bin(binwidth=1, geom="text", aes(label=..count..), hjust = -0.5) +  
+ facet_grid(Nombre.Funciones~.) +
+ labs(title="Funciones", subtitle="Número de empleados por puntuación", x = "Puntuaciones", y = "Número de empleados") +
+ theme_pander() + theme(plot.title = element_text( color = "dodgerblue4", size=25, hjust=0),
+ plot.subtitle = element_text( color = "dodgerblue4", size=20, hjust=0),
+ strip.text.y = element_text(color = "dodgerblue4", size=10), legend.position="none")
+
+
+
+### ### Densidades con facet_grid y nivel requerido
+#Competencias
 
 Nombre.Competencias <- c(rep("Calidad y mejora continua", 55), rep("Pensamiento analítico", 55), rep("Capacidad de aprendizaje", 55),
  rep("Solución de problemas", 55), rep("Competencia digital", 55))
@@ -38,18 +93,22 @@ datos %>% ggplot(aes(Puntuaciones.Competencias, ..count.., fill = Nombre.Compete
  plot.subtitle = element_text( color = "dodgerblue4", size=10, hjust=0),
  strip.text = element_text(color = "dodgerblue4", size=9))
 
-### ### Densidad de Módulos apiladas y rango eje Y fijo
+### ### Densidad de variables apiladas 
+#Modulos
 
 Nombre.Modulos <- c(rep("Competencias", 55), rep("Conocimientos", 55), rep("Funciones", 55))
 Puntuaciones.Modulos <- c(programadores$M1,programadores$M2,programadores$M3)
 
+opuesto <-  function(x){return(-median(x))}
+
 datos <- data.frame(Nombre.Modulos, Puntuaciones.Modulos)
  
-datos %>% ggplot(aes(Puntuaciones.Modulos, ..count.., fill = Nombre.Modulos)) + 
- geom_density( alpha = 0.6, bw = 0.7, color = "white", position="stack") + scale_y_continuous(limit=c(0,20))+
+datos %>% mutate(Nombre.Modulos = reorder(Nombre.Modulos, Puntuaciones.Modulos, FUN = opuesto)) %>% 
+  ggplot(aes(Puntuaciones.Modulos, ..count.., fill = Nombre.Modulos)) + 
+ geom_density( alpha = 0.6, bw = 0.7, color = "white", position="stack")+
  xlab("Puntuaciones")+ylab("Número de empleados")+ labs(title ="Competencias, conocimientos y funciones", subtitle= "Número de empleados por puntuación") +
- scale_fill_discrete(name = "Módulo") + theme_minimal() + theme(plot.title = element_text( color = "dodgerblue4", size=15, hjust=0),
- plot.subtitle = element_text( color = "dodgerblue4", size=10, hjust=0),
- strip.text = element_text(color = "dodgerblue4", size=9))
+ scale_fill_discrete(name = "Módulo") + theme_minimal() + theme(plot.title = element_text( color = "dodgerblue4", size=25, hjust=0),
+ plot.subtitle = element_text( color = "dodgerblue4", size=20, hjust=0))
+
 
 
