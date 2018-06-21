7e1689c9139fc18871272563d8cd2360d9bc16a5
Ariad94 <ariad.94@hotmail.com>
2018-06-21 09:59:57 +0200

    Dispersion facet_grid nivel requerido M1 y M2

--- diagramas_dispersion.r
+++ diagramas_dispersion.r
@@ -13,11 +13,16 @@ Puntuaciones <- c(programadores$M3)
 
 datos <- data.frame(Competencias, Puntuaciones, Módulo)
 
-datos %>% ggplot(aes(Competencias, Puntuaciones)) + geom_point(aes(colour = Módulo), show.legend = TRUE) + 
- geom_smooth(aes(colour = Módulo), size = 0.5, method = "lm", se = FALSE, show.legend = TRUE) + 
+datos %>% ggplot(aes(Competencias, Puntuaciones)) + scale_x_continuous(limits = c(0,100), breaks = seq(0,100, by = 10)) + 
+ scale_y_continuous(limits = c(0,100), breaks = seq(0,100, by = 10)) +
+ geom_point(aes(colour = Módulo), show.legend = FALSE, size = 3) + 
+ geom_smooth(aes(colour = Módulo), size = 1, method = "lm", se = FALSE, show.legend = FALSE) + 
  labs(title="Competencias vs. Desempeño ", x = "Puntuaciones del módulo de competencias", y = "Puntuaciones del módulo de funciones") +
  theme_minimal() + theme(plot.title = element_text( color = "dodgerblue4", size=25, hjust=0),
- strip.text.y = element_text(color = "dodgerblue4", size=20), legend.position="right")
+ axis.title.x = element_text(color = "dodgerblue4", size=13, face = "bold"),
+ axis.title.y = element_text(color = "dodgerblue4", size=13, face = "bold"), axis.text = element_text(size=13))
+
+
 
 # Para M2 vs M1-M3
 
@@ -27,11 +32,60 @@ Puntuaciones <- c(programadores$M1, programadores$M3)
 
 datos <- data.frame(Conocimientos, Puntuaciones, Módulo)
 
-datos %>% ggplot(aes(Conocimientos, Puntuaciones)) + geom_point(aes(colour = Módulo), show.legend = TRUE) + 
- geom_smooth(aes(colour = Módulo), size = 0.5, method = "lm", se = FALSE, show.legend = TRUE) + 
+datos %>% ggplot(aes(Conocimientos, Puntuaciones)) + scale_x_continuous(limits = c(0,100), breaks = seq(0,100, by = 10)) + 
+ scale_y_continuous(limits = c(0,100), breaks = seq(0,100, by = 10)) +
+ geom_point(aes(colour = Módulo), show.legend = TRUE, size = 3) + 
+ geom_smooth(aes(colour = Módulo), size = 1, method = "lm", se = FALSE, show.legend = TRUE) + 
  labs(title="Conocimientos vs. Competencias y Desempeño", x = "Puntuaciones del módulo de conocimientos", y = "Puntuaciones") +
  theme_minimal() + theme(plot.title = element_text( color = "dodgerblue4", size=25, hjust=0),
- strip.text.y = element_text(color = "dodgerblue4", size=20), legend.position="right")
+ axis.title.x = element_text(color = "dodgerblue4", size=13, face = "bold"),
+ axis.title.y = element_text(color = "dodgerblue4", size=13, face = "bold"), legend.title = element_text(color = "dodgerblue4", size=13, face = "bold"),
+ legend.text = element_text(size=13), axis.text = element_text(size=13))
+
+
+
+# Para M1 vs M3 por nivel requerido de competencias
+
+Competencias <- c(programadores$M1)
+Módulo <- c( rep("Funciones",55))
+Puntuaciones <- c(programadores$M3)
+Pordebajo_nivel_requerido <- ifelse(Competencias < 56.25, "Competencias por debajo del nivel requerido", "Competencias por encima o en el nivel requerido")
+
+datos <- data.frame(Competencias, Puntuaciones, Módulo, Pordebajo_nivel_requerido)
+
+datos %>% ggplot(aes(Competencias, Puntuaciones, colour = Pordebajo_nivel_requerido)) + 
+ scale_x_continuous(limits = c(0,100), breaks = seq(0,100, by = 10)) + 
+ scale_y_continuous(limits = c(0,100), breaks = seq(0,100, by = 10)) +
+ geom_point( show.legend = FALSE, size = 3) + 
+ geom_smooth( size = 1, method = "lm", se = FALSE, show.legend = FALSE) + 
+ facet_grid(.~Pordebajo_nivel_requerido) + 
+ labs(title="Competencias vs. Desempeño ", x = "Puntuaciones del módulo de competencias", y = "Puntuaciones del módulo de funciones") +
+ theme_minimal() + theme(plot.title = element_text( color = "dodgerblue4", size=25, hjust=0),
+ axis.title.x = element_text(color = "dodgerblue4", size=13, face = "bold"),
+ axis.title.y = element_text(color = "dodgerblue4", size=13, face = "bold"), axis.text = element_text(size=13),
+ strip.text.x = element_text(color = "dodgerblue4", size=13, face = "bold"))
+
+# Para M2 vs M3 por nivel requerido de competencias
+
+Conocimientos <- c(programadores$M2)
+Módulo <- c( rep("Funciones",55))
+Puntuaciones <- c(programadores$M3)
+Pordebajo_nivel_requerido <- ifelse(Conocimientos < 65, "Conocimientos por debajo del nivel requerido", "Conocimientos por encima o en el nivel requerido")
+
+datos <- data.frame(Conocimientos, Puntuaciones, Módulo, Pordebajo_nivel_requerido)
+
+datos %>% ggplot(aes(Conocimientos, Puntuaciones, colour = Pordebajo_nivel_requerido)) + 
+ scale_x_continuous(limits = c(0,100), breaks = seq(0,100, by = 10)) + 
+ scale_y_continuous(limits = c(0,100), breaks = seq(0,100, by = 10)) +
+ geom_point( show.legend = FALSE, size = 3) + 
+ geom_smooth( size = 1, method = "lm", se = FALSE, show.legend = FALSE) + 
+ facet_grid(.~Pordebajo_nivel_requerido) + 
+ labs(title="Conocimietos vs. Desempeño ", x = "Puntuaciones del módulo de conocimientos", y = "Puntuaciones del módulo de funciones") +
+ theme_minimal() + theme(plot.title = element_text( color = "dodgerblue4", size=25, hjust=0),
+ axis.title.x = element_text(color = "dodgerblue4", size=13, face = "bold"),
+ axis.title.y = element_text(color = "dodgerblue4", size=13, face = "bold"), axis.text = element_text(size=13),
+ strip.text.x = element_text(color = "dodgerblue4", size=13, face = "bold"))
+
 
 
 
